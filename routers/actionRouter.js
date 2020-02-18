4d5808990c82a4fdc6e16407bccbac3f1a0f5c22
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-17 19:20:08 -0500

    npx knex seed:run

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const Actions = require('../data/helpers/actionModel');
 
-const router = express.Router({
-    mergeParams: true
-});
+const router = express.Router();
 
 router.get('/', (req, res) => {
     Actions
     .get()
-    .then(action => {
-        res.status(200).json(action)
+    .then(actions => {
+        res.status(200).json(actions)
     })
     .catch(err => {
         console.log(err)
--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Projects = require('../data/helpers/projectModel');
-const Actions = require('../data/helpers/actionModel');
+// const Actions = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
